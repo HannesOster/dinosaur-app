c71113d6b221d16036b07e6f576aa27e30298618
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-30 21:58:53 +0200

    destructure dinsosaur

--- src/pages/species/[id].js
+++ src/pages/species/[id].js
@@ -3,5 +3,7 @@ import useRouter from "next/router";
 
 export default function Species() {
   const router = useRouter();
-  const id = router.query;
+  const { id } = router.query;
+  const dinosaur = dinosaurs.find((dinosaur) => dinosaur.id === id);
+  const { name, description, size, carnivore, extinction, enemies } = dinosaur;
 }
