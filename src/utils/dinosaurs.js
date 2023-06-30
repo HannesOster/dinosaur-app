713951829f2dc9b9b4016c219ac6355eeb2d5bbc
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-30 21:23:38 +0200

    add dinosaur list

---
+++ src/utils/dinosaurs.js
@@ -0,0 +1,341 @@
+export const dinosaurs = [
+  {
+    id: "1",
+
+    name: "Tyrannosaurus Rex",
+
+    description:
+      "The Tyrannosaurus Rex, also known as T-Rex, was one of the largest carnivorous dinosaurs.",
+
+    size: 12,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Triceratops, Ankylosaurus",
+  },
+
+  {
+    id: "2",
+
+    name: "Velociraptor",
+
+    description:
+      "Velociraptor was a small and fast dinosaur with a distinctive sickle-shaped claw on each hindfoot.",
+
+    size: 1,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Protoceratops, Oviraptor",
+  },
+
+  {
+    id: "3",
+
+    name: "Stegosaurus",
+
+    description:
+      "The Stegosaurus was a large herbivorous dinosaur with distinctive plates and spikes on its back.",
+
+    size: 9,
+
+    carnivore: false,
+
+    extinction: "Jurassic",
+
+    enemies: "Allosaurus, Ceratosaurus",
+  },
+
+  {
+    id: "4",
+
+    name: "Triceratops",
+
+    description:
+      "The Triceratops was a large herbivorous dinosaur with three horns on its head and a bony frill.",
+
+    size: 8,
+
+    carnivore: false,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "5",
+
+    name: "Brachiosaurus",
+
+    description:
+      "The Brachiosaurus was one of the tallest and largest dinosaurs ever discovered.",
+
+    size: 23,
+
+    carnivore: false,
+
+    extinction: "Jurassic",
+
+    enemies: "Ceratosaurus, Allosaurus",
+  },
+
+  {
+    id: "6",
+
+    name: "Dilophosaurus",
+
+    description:
+      "The Dilophosaurus was a medium-sized dinosaur known for the pair of crests on its head.",
+
+    size: 7,
+
+    carnivore: true,
+
+    extinction: "Jurassic",
+
+    enemies: "Brachiosaurus, Stegosaurus",
+  },
+
+  {
+    id: "7",
+
+    name: "Pteranodon",
+
+    description:
+      "The Pteranodon was a large flying reptile with a long crest on its head.",
+
+    size: 6,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Mosasaurus, Tylosaurus",
+  },
+
+  {
+    id: "8",
+
+    name: "Ankylosaurus",
+
+    description:
+      "The Ankylosaurus was a heavily armored dinosaur with a large club-like tail.",
+
+    size: 10,
+
+    carnivore: false,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "9",
+
+    name: "Archaeopteryx",
+
+    description:
+      "The Archaeopteryx was a small dinosaur that shared both avian and reptilian characteristics.",
+
+    size: 0.5,
+
+    carnivore: true,
+
+    extinction: "Jurassic",
+
+    enemies: "Compsognathus, Microraptor",
+  },
+
+  {
+    id: "10",
+
+    name: "Spinosaurus",
+
+    description:
+      "The Spinosaurus was a giant carnivorous dinosaur with a sail-like structure on its back.",
+
+    size: 18,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "11",
+
+    name: "Allosaurus",
+
+    description:
+      "The Allosaurus was a large carnivorous dinosaur with sharp teeth and three-fingered hands.",
+
+    size: 9,
+
+    carnivore: true,
+
+    extinction: "Jurassic",
+
+    enemies: "Stegosaurus, Brachiosaurus",
+  },
+
+  {
+    id: "12",
+
+    name: "Plesiosaurus",
+
+    description:
+      "The Plesiosaurus was a marine reptile that lived during the Jurassic period.",
+
+    size: 11,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tylosaurus, Mosasaurus",
+  },
+
+  {
+    id: "13",
+
+    name: "Parasaurolophus",
+
+    description:
+      "The Parasaurolophus was a herbivorous dinosaur with a long backwards-curving crest on its head.",
+
+    size: 9,
+
+    carnivore: false,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "14",
+
+    name: "Carnotaurus",
+
+    description:
+      "The Carnotaurus was a large carnivorous dinosaur with distinctive horns above its eyes.",
+
+    size: 9,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Triceratops, Ankylosaurus",
+  },
+
+  {
+    id: "15",
+
+    name: "Iguanodon",
+
+    description:
+      "The Iguanodon was a large herbivorous dinosaur with a spiked thumb and a toothless beak.",
+
+    size: 10,
+
+    carnivore: false,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "16",
+
+    name: "Ceratosaurus",
+
+    description:
+      "The Ceratosaurus was a large carnivorous dinosaur with a prominent horn on its nose.",
+
+    size: 8,
+
+    carnivore: true,
+
+    extinction: "Jurassic",
+
+    enemies: "Stegosaurus, Brachiosaurus",
+  },
+
+  {
+    id: "17",
+
+    name: "Pachycephalosaurus",
+
+    description:
+      "The Pachycephalosaurus was a herbivorous dinosaur with a thick, bony dome on its head.",
+
+    size: 6,
+
+    carnivore: false,
+
+    extinction: "Cretaceous",
+
+    enemies: "Tyrannosaurus Rex, Giganotosaurus",
+  },
+
+  {
+    id: "18",
+
+    name: "Mosasaurus",
+
+    description:
+      "The Mosasaurus was a large marine reptile that lived during the Late Cretaceous period.",
+
+    size: 18,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Pteranodon, Tylosaurus",
+  },
+
+  {
+    id: "19",
+
+    name: "Brontosaurus",
+
+    description:
+      "The Brontosaurus was a large herbivorous dinosaur with a long neck and a long whip-like tail.",
+
+    size: 22,
+
+    carnivore: false,
+
+    extinction: "Jurassic",
+
+    enemies: "Ceratosaurus, Allosaurus",
+  },
+
+  {
+    id: "20",
+
+    name: "Giganotosaurus",
+
+    description:
+      "The Giganotosaurus was a giant carnivorous dinosaur that roamed during the Late Cretaceous period.",
+
+    size: 13,
+
+    carnivore: true,
+
+    extinction: "Cretaceous",
+
+    enemies: "Triceratops, Ankylosaurus",
+  },
+];
