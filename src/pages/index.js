d8adcf581be9d47ac05f4a30d1158af566e2b0ae
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-30 21:45:47 +0200

    setup id.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { dinosaurs } from "../utils/dinosaurs";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,12 @@ export default function Home() {
       </Head>
       <ul>
         {dinosaurs.map((dinosaur) => {
-          return <li key={dinosaur.id}>{dinosaur.name}</li>;
+          return (
+            <li key={dinosaur.id}>
+              {dinosaur.name}
+              <Link href={`/species/${dinosaur.id}`}>Details</Link>
+            </li>
+          );
         })}
       </ul>
     </>
