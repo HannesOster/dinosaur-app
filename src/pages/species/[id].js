b2e8c70a49732faa39a7fbeddf73f26313a8fee6
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-06-30 22:53:02 +0200

    finish component species

--- src/pages/species/[id].js
+++ src/pages/species/[id].js
@@ -6,4 +6,18 @@ export default function Species() {
   const { id } = router.query;
   const dinosaur = dinosaurs.find((dinosaur) => dinosaur.id === id);
   const { name, description, size, carnivore, extinction, enemies } = dinosaur;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <p>Size : {size}</p>
+      <label>
+        Carnivore
+        <input type="checkbox" checked={carnivore} />
+      </label>
+      <p>Extinction: {extinction}</p>
+      <p>Enemies: {enemies}</p>
+    </div>
+  );
 }
